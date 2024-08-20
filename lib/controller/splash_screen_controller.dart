0329411dcd059d8ec1adee2bcb7deb2914238d05
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-08-20 15:56:11 +0800

    aaa

--- lib/controller/splash_screen_controller.dart
+++ lib/controller/splash_screen_controller.dart
@@ -1,8 +1,7 @@
-import 'package:multi_sport_score/ui/screens/bottom_navigation/bottom_navigation_menu.dart';
 import 'package:get/get.dart';
 import 'package:multi_sport_score/ui/screens/intro/intro_screen.dart';
 
-class SplashScreenController extends GetxController{
+class SplashScreenController extends GetxController {
   @override
   void onInit() {
     super.onInit();
--- lib/ui/screens/intro/intro_screen.dart
+++ lib/ui/screens/intro/intro_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:multi_sport_score/constants/color_const.dart';
 import 'package:multi_sport_score/constants/dimen_const.dart';
 import 'package:multi_sport_score/constants/images_const.dart';
 import 'package:multi_sport_score/ui/custom_widgets/custom_follow_widget.dart';
 import 'package:multi_sport_score/ui/custom_widgets/custom_text.dart';
+
+import '../bottom_navigation/bottom_navigation_menu.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
@@ -14,113 +17,133 @@ class IntroScreen extends StatelessWidget {
     return DefaultTabController(
       length: 4, // Number of tabs
       child: Scaffold(
-        backgroundColor: primaryColor,
-        appBar: AppBar(
-          toolbarHeight: 100.h,
           backgroundColor: primaryColor,
-          title: CustomText(
-            text: "Select one or more Leagues to follow",
-            fontWeight: FontWeight.bold,
-            fontSize: 18.sp,
-            color: whiteColor,
-            maxLines: 2,
-          ),
-          bottom: TabBar(
-            dividerColor: primaryColor,
-
-            indicatorColor: secondaryColor,
-            labelColor: secondaryColor,
-            unselectedLabelColor: lightWhiteColor,
-            tabs: const [
-              Tab(text: 'Popular'),
-              Tab(text: 'Football'),
-              Tab(text: 'Basketball'),
-              Tab(text: 'Tennis'),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 3.w,vertical: 10.h),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CustomText(text: " Top",fontSize: 15.sp,fontWeight: FontWeight.bold,color: greyColor,),
-                  kSizedBoxH10,
-                  Expanded(
-                    child: GridView.builder(
-                        itemCount: 9,
-                        gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                        mainAxisSpacing: 0.w,
-                          crossAxisSpacing: 0.h,
-                          childAspectRatio: 1.2,
-                        ),
-                        itemBuilder: (context, index) {
-                         return const CustomFollowWidget(label: "Premier League", image: logoImg, subLabel: "England");
-
-                        }),
-                  ),
-                ],
-              ),
+          appBar: AppBar(
+            toolbarHeight: 100.h,
+            backgroundColor: primaryColor,
+            title: CustomText(
+              text: "Select one or more Leagues to follow",
+              fontWeight: FontWeight.bold,
+              fontSize: 18.sp,
+              color: whiteColor,
+              maxLines: 2,
             ),
-            GridView.builder(
-                itemCount: 9,
-                gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 0.w,
-                  crossAxisSpacing: 0.h,
-                  childAspectRatio: 1.2,
-                ),
-                itemBuilder: (context, index) {
-                  return const CustomFollowWidget(label: "Premier League", image: logoImg, subLabel: "England");
-
-                }),
-            GridView.builder(
-                itemCount: 9,
-                gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 0.w,
-                  crossAxisSpacing: 0.h,
-                  childAspectRatio: 1.2,
-                ),
-                itemBuilder: (context, index) {
-                  return const CustomFollowWidget(label: "Premier League", image: logoImg, subLabel: "England");
-
-                }),
-            GridView.builder(
-                itemCount: 9,
-                gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 0.w,
-                  crossAxisSpacing: 0.h,
-                  childAspectRatio: 1.2,
-                ),
-                itemBuilder: (context, index) {
-                  return const CustomFollowWidget(label: "Premier League", image: logoImg, subLabel: "England");
-
-                }),
-          ],
-        ),
-        bottomNavigationBar: Container(
-          height: 50.h,
-          color: primaryColor,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+            bottom: TabBar(
+              dividerColor: primaryColor,
+              indicatorColor: secondaryColor,
+              labelColor: secondaryColor,
+              unselectedLabelColor: lightWhiteColor,
+              tabs: const [
+                Tab(text: 'Popular'),
+                Tab(text: 'Football'),
+                Tab(text: 'Basketball'),
+                Tab(text: 'Tennis'),
+              ],
+            ),
+          ),
+          body: TabBarView(
             children: [
-              CustomText(
-                text: "Next",
-                color: secondaryColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 16.sp,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 10.h),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomText(
+                      text: " Top",
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.bold,
+                      color: greyColor,
+                    ),
+                    kSizedBoxH10,
+                    Expanded(
+                      child: GridView.builder(
+                          itemCount: 9,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 0.w,
+                            crossAxisSpacing: 0.h,
+                            childAspectRatio: 1.2,
+                          ),
+                          itemBuilder: (context, index) {
+                            return const CustomFollowWidget(
+                                label: "Premier League",
+                                image: logoImg,
+                                subLabel: "England");
+                          }),
+                    ),
+                  ],
+                ),
               ),
-              Icon(Icons.arrow_forward_ios, color: secondaryColor,size: 20.w,)
+              GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 0.w,
+                    crossAxisSpacing: 0.h,
+                    childAspectRatio: 1.2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return const CustomFollowWidget(
+                        label: "Premier League",
+                        image: logoImg,
+                        subLabel: "England");
+                  }),
+              GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 0.w,
+                    crossAxisSpacing: 0.h,
+                    childAspectRatio: 1.2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return const CustomFollowWidget(
+                        label: "Premier League",
+                        image: logoImg,
+                        subLabel: "England");
+                  }),
+              GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 0.w,
+                    crossAxisSpacing: 0.h,
+                    childAspectRatio: 1.2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return const CustomFollowWidget(
+                        label: "Premier League",
+                        image: logoImg,
+                        subLabel: "England");
+                  }),
             ],
           ),
-        )
-      ),
-
+          bottomNavigationBar: Container(
+            height: 50.h,
+            color: primaryColor,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Get.to(() => BottomNavigationMenu());
+                  },
+                  child: CustomText(
+                    text: "Next",
+                    color: secondaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.sp,
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: secondaryColor,
+                  size: 20.w,
+                )
+              ],
+            ),
+          )),
     );
   }
 }
